89f8575ddf850b563f77156e50735ef3ca98e858
YuZhongFanXing <YuZhongFanXing@users.noreply.github.com>
2026-09-07 08:34:25 +0000

    deploy: 5ffb733ed8ff946cd50d897f2220abb8b0e51f7f

--- config.js
+++ config.js
@@ -1,4 +1,1 @@
-window.SWJTU_MATH_CONFIG = {
-  // 部署上传 Worker 后填写，例如：https://swjtu-math-api.example.workers.dev
-  apiBase: ""
-};
+window.SWJTU_MATH_CONFIG = { apiBase: "https://swjtu-math-api.15797612695.workers.dev" };
